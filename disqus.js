e1a36c4fbaba9d5cec91815d72610c83ce28ad7b
Rob Loach <robloach@gmail.com>
2010-02-08 05:16:48 +0000

    Drupal 7 port of the Disqus module.

--- disqus.js
+++ disqus.js
@@ -1,25 +1,30 @@
 // $Id$
+(function ($) {
 
 /**
- * Drupal Disqus behaviors.
+ * Drupal Disqus behavior.
  */
-Drupal.behaviors.disqus = function(context) {
-  if (Drupal.settings.disqusCommentDomain || false) {
-    // Create the query.
-    var query = '?';
-    jQuery("a[href$='#disqus_thread']").each(function(i) {
-      query += 'url' + i + '=' + encodeURIComponent($(this).attr('href')) + '&';
-    });
-
-    // Make sure we are actually processing some links.
-    if (query.length > 2) {
-      // Make the AJAX call to get the number of comments.
-      jQuery.ajax({
-        type: 'GET',
-        url: 'http://disqus.com/forums/' + Drupal.settings.disqusCommentDomain + '/get_num_replies.js' + query,
-        dataType: 'script',
-        cache: true
+Drupal.behaviors.disqus = {
+  attach: function (context) {
+    if (Drupal.settings.disqusCommentDomain || false) {
+      // Create the query.
+      var query = '?';
+      jQuery("a[href$='#disqus_thread']").each(function(i) {
+        query += 'url' + i + '=' + encodeURIComponent($(this).attr('href')) + '&';
       });
+
+      // Make sure we are actually processing some links.
+      if (query.length > 2) {
+        // Make the AJAX call to get the number of comments.
+        jQuery.ajax({
+          type: 'GET',
+          url: 'http://disqus.com/forums/' + Drupal.settings.disqusCommentDomain + '/get_num_replies.js' + query,
+          dataType: 'script',
+          cache: true
+        });
+      }
     }
   }
 };
+
+})(jQuery);

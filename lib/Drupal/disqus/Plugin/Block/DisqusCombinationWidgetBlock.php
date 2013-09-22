5d0c89faa640521e791637e4eab723fbe735f725
Andy Truong <thehongtt@gmail.com>
2013-09-22 10:22:51 +0700

    Porting block: Add $id property to blocks.

--- lib/Drupal/disqus/Plugin/Block/DisqusCombinationWidgetBlock.php
+++ lib/Drupal/disqus/Plugin/Block/DisqusCombinationWidgetBlock.php
@@ -14,6 +14,8 @@ use Drupal\Core\Annotation\Translation;
  * )
  */
 class DisqusCombinationWidgetBlock extends DisqusBaseBlock {
+  protected $id = 'disqus_combination_widget';
+
   /**
    * {@inheritdoc}
    */
